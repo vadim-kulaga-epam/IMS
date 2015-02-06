aa85437f6cf2f283bc868ab9d464d70081ea766c
diralf <diralfeastarea@gmail.com>
2015-02-06 15:46:47 +0300

    Server change Express is installed in request handler

--- app.js
+++ app.js
@@ -6,52 +6,25 @@ var WebSocketServer = require('ws').Server
 var http = require("http"),
     url = require("url"),
     path = require("path"),
-    fs = require("fs")
+    fs = require("fs"),
+    express = require('express');
     
 var router = require("./router"),
-    routesHandler = require("./routesHandler")
+    routes = require("./routesHandler")
     
+var app = express();    
 
-var server = http.createServer(function(request, response) {
- 
-//  var uri = url.parse(request.url).pathname
-//    , filename = path.join(process.cwd() + '/public', uri);
+app.set('port', port );
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.bodyParser());
 
-  router(routesHandler.handle, request, response); 
+app.get('/test', routes.supplies);
 
-//  var contentTypesByExtension = {
-//    '.html': "text/html",
-//    '.css':  "text/css",
-//    '.js':   "text/javascript"
-//  };
-//
-//  path.exists(filename, function(exists) {
-//    if(!exists) {
-//      response.writeHead(404, {"Content-Type": "text/plain"});
-//      response.write("404 Not Found\n");
-//      response.end();
-//      return;
-//    }
-//
-//    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
-//
-//    fs.readFile(filename, "binary", function(err, file) {
-//      if(err) {        
-//        response.writeHead(500, {"Content-Type": "text/plain"});
-//        response.write(err + "\n");
-//        response.end();
-//        return;
-//      }
-//      
-//      var headers = {};
-//      var contentType = contentTypesByExtension[path.extname(filename)];
-//      if (contentType) headers["Content-Type"] = contentType;
-//      response.writeHead(200, headers);
-//      response.write(file, "binary");
-//      response.end();
-//    });
-//   });
-});
+var server = http.createServer(app);
 
 server.listen( port, ipaddress, function() {
     console.log((new Date()) + ' Server is listening on port 8080');
--- routesHandler/index.js
+++ routesHandler/index.js
@@ -1,7 +1,8 @@
 var handle = {};
 
 //handle["/views/supplies.html"] = require("./suppliesRoute");
-handle["/test"] = require("./suppliesRoute");
+//handle["/test"] = require("./suppliesRoute");
 
-exports.handle = handle;
+//exports.handle = handle;
+exports.supplies = require("./suppliesRoute");
 
